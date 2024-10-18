4629c1aec81e1da47d8d8c6d8a1fcb5f4e263c01
invictus841 <usedforfunny@gmail.com>
2024-10-18 10:34:15 +0200

    Fichier ViewModelTests. Pas de tests pour l'instant.

---
+++ UserListTests/ViewModelTests.swift
@@ -0,0 +1,19 @@
+//
+//  ViewModelTests.swift
+//  UserListTests
+//
+//  Created by Alexandre Talatinian on 18/10/2024.
+//
+
+import XCTest
+@testable import UserList
+
+struct ViewModelTests {
+
+//    @Test func <#test function name#>() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+
+    
+    
+}
